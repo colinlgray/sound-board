528e4bdc8a757a46c5c8c6e0b2a41f7fa3b22379
Colin Gray <colin@cgray.dev>
2019-08-31 12:13:04 -0700

    Started messing around with magenta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as mm from "@magenta/music";
+import "./App.css";
+
+const DRUMS = {
+  notes: [
+    { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
+    { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
+    { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
+    { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
+    { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 36, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
+    { pitch: 42, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
+    { pitch: 46, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
+    { pitch: 42, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
+    { pitch: 48, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
+    { pitch: 50, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true }
+  ],
+  quantizationInfo: { stepsPerQuarter: 4 },
+  tempos: [{ time: 0, qpm: 120 }],
+  totalQuantizedSteps: 11
+};
+
+const player = new mm.Player();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          player.start(DRUMS);
+        }}
+      >
+        click
+      </button>
     </div>
   );
 }
