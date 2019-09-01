048e5eee0c7a82f5993b49edf8cecea078684c91
Colin Gray <colin@cgray.dev>
2019-08-31 22:58:33 -0700

    shuffled the deckchairs

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React from "react";
-import * as mm from "@magenta/music";
+import Board from "./components/Board";
 import "./App.css";
-
-const DRUMS = {
-  notes: [
-    { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
-    { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
-    { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-    { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-    { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 36, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-    { pitch: 42, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-    { pitch: 46, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-    { pitch: 42, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-    { pitch: 48, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-    { pitch: 50, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true }
-  ],
-  quantizationInfo: { stepsPerQuarter: 4 },
-  tempos: [{ time: 0, qpm: 120 }],
-  totalQuantizedSteps: 11
-};
-
-const player = new mm.Player();
 
 function App() {
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          player.start(DRUMS);
-        }}
-      >
-        click
-      </button>
+      <Board />
     </div>
   );
 }
