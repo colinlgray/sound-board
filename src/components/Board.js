8e52fed002800ba57d980c434788153e6f21ae0a
Colin Gray <colin@cgray.dev>
2019-09-02 10:53:38 -0700

    some new folders

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,45 +1,16 @@
 import React from "react";
-import * as mm from "@magenta/music";
+import Button from "./Button";
 
-const DRUMS = {
-  notes: [
-    { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-    { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
-    { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
-    { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-    { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-    { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-    { pitch: 36, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-    { pitch: 42, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-    { pitch: 46, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-    { pitch: 42, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-    { pitch: 48, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-    { pitch: 50, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true }
-  ],
-  quantizationInfo: { stepsPerQuarter: 4 },
-  tempos: [{ time: 0, qpm: 120 }],
-  totalQuantizedSteps: 11
-};
-
-const player = new mm.Player();
+const size = 5;
 
 export default function Board() {
-  return (
-    <div>
-      Board goes here
-      <button
-        onClick={() => {
-          player.start(DRUMS);
-        }}
-      >
-        click
-      </button>
-    </div>
-  );
+  const domEl = [];
+  for (let i = 0; i < size; i++) {
+    let inner = [];
+    for (let j = 0; j < size; j++) {
+      inner.push(<Button row={i} column={j} />);
+    }
+    domEl.push(inner);
+  }
+  return <div>{domEl}</div>;
 }
---
+++ src/utils/Player.js
@@ -0,0 +1,32 @@
+import * as mm from "@magenta/music";
+
+const DRUMS = {
+  notes: [
+    { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
+    { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
+    { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
+    { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
+    { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
+    { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+    { pitch: 36, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
+    { pitch: 42, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
+    { pitch: 46, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
+    { pitch: 42, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
+    { pitch: 48, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
+    { pitch: 50, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true }
+  ],
+  quantizationInfo: { stepsPerQuarter: 4 },
+  tempos: [{ time: 0, qpm: 120 }],
+  totalQuantizedSteps: 11
+};
+
+export const player = new mm.Player();
+export const play = () => {
+  player.play(DRUMS);
+};
