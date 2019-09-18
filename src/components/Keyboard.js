dc025836546a397b0a524ea31914c06b1c4c74ba
Colin Gray <colin@cgray.dev>
2019-09-17 17:33:01 -0700

    Styled the keyboard a bit

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,35 +1,44 @@
 import React from "react";
 import clsx from "clsx";
 
+const classes = {
+  white: "h-40 z-0 bg-gray-100",
+  black: "h-20 z-10 bg-gray-800"
+};
+
 function Key(props) {
   return (
     <div
       className={clsx(
-        "w-4 m-2",
-        { "h-20": props.color === "white" },
-        { "h-10": props.color === "black" }
+        "border-2 border-gray w-8",
+        { "-ml-4": !props.first },
+        props.className,
+        classes[props.color]
       )}
       role="button"
-      style={{ background: props.color, border: "1px solid gray" }}
     />
   );
 }
 
 function KeyGroup(props) {
-  const els = [];
-  els.push(<Key color="white" key={`key-`} />);
-  for (let index = 0; index < props.size; index++) {
-    els.push(<Key color="black" key={`key-${index * props.size + index}`} />);
-    els.push(
-      <Key color="white" key={`key-${index * props.size + index + 1}`} />
-    );
-  }
-  return els;
+  return (
+    <>
+      <Key color="white" first />
+      {new Array(props.size).fill(0).map((_, index) => (
+        <React.Fragment key={`group-${index}`}>
+          <Key color="black" key={`key-${index}`} />
+          <Key color="white" key={`key-${index + 1}`} />
+        </React.Fragment>
+      ))}
+    </>
+  );
 }
 
 export default function Keyboard() {
   return (
     <div className="flex">
+      <KeyGroup size={2} />
+      <KeyGroup size={3} />
       <KeyGroup size={2} />
       <KeyGroup size={3} />
     </div>
